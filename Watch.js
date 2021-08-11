3a72d676fc1453f498234925bd836b101759f3bd
OA Wu <comdan66@gmail.com>
2021-08-11 15:53:05 +0800

    fix

--- Watch.js
+++ Watch.js
@@ -16,10 +16,19 @@ const { create: Queue }     = require('@oawu/queue')
 
 const rootPath = Path.resolve(__dirname, ('..' + Path.sep).repeat(0)) + Path.sep
 
+const pad0 = n => n < 10 ? `0${n}` : n
+
+const now = _ => {
+  const date = new Date(), y = date.getFullYear(), m = pad0(date.getMonth() + 1), d = pad0(date.getDate()), h = pad0(date.getHours()), i = pad0(date.getMinutes()), s = pad0(date.getSeconds())
+  return [[y, m, d].join('-'), [h, i, s].join(':')].join(' ')
+}
+
 const doing = (platform, showLog, time) => Queue()
 
+  .enqueue(next => next(showLog && process.stdout.write("\r\n" + ' 執行檢查：' + now() + "\r\n")))
+
   .enqueue(next => IP((error, { privateIP: private, publicIP: public } = {}) => error
-    ? setTimeout(_ => doing(platform, showLog, time), time, process.stdout.write("\r" + error.message + "\n"))
+    ? setTimeout(_ => doing(platform, showLog, time), time, showLog && process.stdout.write("\r" + error.message + "\n"))
     : next({private, public}), showLog))
 
   .enqueue((next, ip) => FileSystem.readFile(rootPath + platform.cache, 'utf8', (error, data) => error || data !== ip.public
@@ -27,11 +36,13 @@ const doing = (platform, showLog, time) => Queue()
     : setTimeout(_ => doing(platform, showLog, time), time, showLog && done('沒有，不需要動作')), showLog && title('讀取快取，確認是否有改變過')))
 
   .enqueue((next, ip) => platform.upload((error, data) => error
-    ? setTimeout(_ => doing(platform, showLog, time), time, process.stdout.write("\r" + error.message + "\n"))
+    ? setTimeout(_ => doing(platform, showLog, time), time, showLog && process.stdout.write("\r" + error.message + "\n"))
     : next(ip, platform), showLog))
 
   .enqueue((next, ip) => FileSystem.writeFile(rootPath + platform.cache, ip.public, 'utf8', error => error
-    ? next(process.stdout.write("\r" + error.message + "\n"), showLog && fail('失敗'))
+    ? next(showLog && process.stdout.write("\r" + error.message + "\n"), showLog && fail('失敗'))
     : next(ip, showLog && done()), showLog && title('儲存快取')))
+
+  .enqueue(next => next(setTimeout(_ => doing(platform, showLog, time), time)))
 
 module.exports = (platform, time = 60 * 1000, showLog = true) => platform instanceof Platform && doing(platform, showLog, time)
